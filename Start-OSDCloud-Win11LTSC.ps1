7be9c2d610818a7d0ea8dfe90f73ba828eb23e1d
Matt-Does-Endpoint <130109641+Matt-Does-Endpoint@users.noreply.github.com>
2025-01-10 17:34:32 -0800

    Update Start-OSDCloud-Win11LTSC.ps1

--- Start-OSDCloud-Win11LTSC.ps1
+++ Start-OSDCloud-Win11LTSC.ps1
@@ -1,6 +1,5 @@
 #Variables to define the Windows OS / Edition etc to be applied during OSDCloud
 $OSName = 'D:\OSDCloud\OS\Install.wim'
-$OSEdition = 'Windows 11 Enterprise LTSC'
 $OSImageIndex = '1'
 $OSLanguage = 'en-us'
 $OSActivation = 'Retail'
@@ -21,4 +20,4 @@ $Global:MyOSDCloud = [ordered]@{
 Write-Host "Starting OSDCloud" -ForegroundColor Green
 write-host "Start-OSDCloud -OSImageIndex $OSImageIndex"
 
-Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+Start-OSDCloud -OSName $OSName -OSActivation $OSActivation -OSLanguage $OSLanguage
