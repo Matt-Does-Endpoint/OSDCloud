ef9255a83774a5170501e3bc58a69b2e6620d6bb
Matt-Does-Endpoint <130109641+Matt-Does-Endpoint@users.noreply.github.com>
2025-01-10 19:38:54 -0800

    Update Start-OSDCloud-Win11LTSC.ps1

--- Start-OSDCloud-Win11LTSC.ps1
+++ Start-OSDCloud-Win11LTSC.ps1
@@ -22,4 +22,5 @@ Write-Host "Starting OSDCloud" -ForegroundColor Green
 write-host "Start-OSDCloud -OSName Windows 11 LTSC -OSLanguage $OSLanguage"
 
 #Launch OSDCloud
-Start-OSDCloud -FindImageFile | where-object -eq "Win11LTSC.WIM"
+Start-OSDCloud -FindImageFile
+#| where-object -eq "Win11LTSC.WIM"
