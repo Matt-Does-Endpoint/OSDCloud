37fd5484451e7bc0bf56bd86600e3243baa7148e
Matt-Does-Endpoint <130109641+Matt-Does-Endpoint@users.noreply.github.com>
2025-01-10 19:36:01 -0800

    Update Start-OSDCloud-Win11LTSC.ps1

--- Start-OSDCloud-Win11LTSC.ps1
+++ Start-OSDCloud-Win11LTSC.ps1
@@ -1,7 +1,5 @@
 #Variables to define the Windows OS / Edition etc to be applied during OSDCloud
 $OSLanguage = 'en-us'
-$ImageFileURL = 'D:\OSDCloud\OS\Win11LTSC.wim'
-$OSImageIndex = '1'
 
 #Set OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
@@ -13,10 +11,10 @@ $Global:MyOSDCloud = [ordered]@{
     WindowsDefenderUpdate = [bool]$true
     ClearDiskConfirm = [bool]$False
     ShutdownSetupComplete = [bool]$false
-    OSImageIndex = '1'
     ImageFileItem = 'D:\OSDCloud\OS\Win11LTSC.wim'
     ImageFileFullName = 'D:\OSDCloud\OS\Win11LTSC.wim'
     ImageFileName = 'Win11LTSC.wim'
+    OSImageIndex = '1'
 }
 
 #Launch OSDCloud
