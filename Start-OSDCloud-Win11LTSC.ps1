c930446b2c6ae4cbc3acf81a03e33fc6a8ff6e02
Matt-Does-Endpoint <130109641+Matt-Does-Endpoint@users.noreply.github.com>
2025-01-22 11:36:51 -0800

    Update Start-OSDCloud-Win11LTSC.ps1

--- Start-OSDCloud-Win11LTSC.ps1
+++ Start-OSDCloud-Win11LTSC.ps1
@@ -18,10 +18,10 @@ $Global:MyOSDCloud = [ordered]@{
 }
 
 #modify ps module to support local image selection
-#$OSDSCript = Get-Item -Path "X:\Program Files\WindowsPowerShell\Modules\OSD\*\Public\OSDCloud.ps1"
-#$ScriptBody = Get-Content $OSDSCript
-#$newValue = $ScriptBody -replace "$Global:OSDCloud.ImageFileItem.Fullname", "$Global:OSDCloud.ImageFileItem"
-#Set-Content -Path "$OSDSCript" -Value $newValue -Force
+$OSDSCript = Get-Item -Path "X:\Program Files\WindowsPowerShell\Modules\OSD\*\Public\OSDCloud.ps1"
+$ScriptBody = Get-Content $OSDSCript
+$newValue = $ScriptBody -replace "$Global:OSDCloud.ImageFileItem.Fullname", "$Global:OSDCloud.ImageFileItem"
+Set-Content -Path "$OSDSCript" -Value $newValue -Force
 
 #Launch OSDCloud
 #
