c70e963e3a4fc2d384f9dae89b2e2a54a517b46b
Matt-Does-Endpoint <130109641+Matt-Does-Endpoint@users.noreply.github.com>
2025-01-22 15:20:37 -0800

    Update Start-OSDCloud-Win11LTSC.ps1

--- Start-OSDCloud-Win11LTSC.ps1
+++ Start-OSDCloud-Win11LTSC.ps1
@@ -12,7 +12,6 @@ $Global:MyOSDCloud = [ordered]@{
     ClearDiskConfirm = [bool]$False
     ShutdownSetupComplete = [bool]$false
     ImageFileItem = 'D:\OSDCloud\OS\Win11LTSC.wim'
-    ImageFileItem.Fullname = 'D:\OSDCloud\OS\Win11LTSC.wim'
     ImageFileFullName = 'D:\OSDCloud\OS\Win11LTSC.wim'
     ImageFileName = 'Win11LTSC.wim'
     OSImageIndex = '1'
